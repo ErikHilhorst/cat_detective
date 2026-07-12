25458e10e5f4e7fb9863db9f0b8199bc3206408f
ErikHilhorst <erik.hilhorst87@gmail.com>
2026-07-12 12:51:17 +0200

    added images

--- obj/Debug/net8.0/CatDetective.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatDetective.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatDetective")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567e302308472f5ae2838c27eac0d0434b72037b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66208af07a473bebfc6180fd5a525b9db47e63b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatDetective")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatDetective")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
